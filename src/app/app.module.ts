0ee246a74dd95cde6edf47a7ce0ed9cadb7232d0
Avani14 <trivediavani14@gmail.com>
2022-09-14 19:43:18 +0530

    First commit. Added the form component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpFormComponent } from './components/emp-form/emp-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpFormComponent
   ],
   imports: [
     BrowserModule,
